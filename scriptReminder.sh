1d57bace36794f30ef81af42d688afa7b7c00a5d
ShahzamanRai <108186350+ShahzamanRai@users.noreply.github.com>
2022-07-19 21:45:11 +0500

    Main Script
    Script to show notification and ring after 20 second

--- scriptReminder.sh
+++ scriptReminder.sh
@@ -5,10 +5,10 @@ do
 
     notify-send.sh --urgency=critical --expire-time=3000 -f --app-name=Reminder "Reminder Starts."
     sleep 1200
-    aplay /home/shahzaman/Documents/Notify/bell.wav
+    aplay PATH_TO_bell.wav_FILE/bell.wav
     notify-send.sh --urgency=critical --expire-time=20000 -f --app-name=Reminder "Take a break" "Take a break for 20 second and look an object 20m far"
     sleep 20
-    aplay /home/shahzaman/Documents/Notify/bell.wav
+    aplay PATH_TO_bell.wav_FILE/bell.wav
 
 
 done
