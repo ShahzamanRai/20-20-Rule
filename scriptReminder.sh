449bd7ff8ecb3fb1c5b065dea4d21a95454e348b
Shah (SR) Zaman <shahzamanrai8@gmail.com>
2023-08-06 14:04:19 +0500

    Update scriptReminder.sh

--- scriptReminder.sh
+++ scriptReminder.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-
+ notify-send --urgency=normal --expire-time=3000 --app-name=Reminder "Reminder Starts."
 while :
 do
-
-    notify-send.sh --urgency=critical --expire-time=3000 -f --app-name=Reminder "Reminder Starts."
     sleep 1200
-    aplay PATH_TO_bell.wav_FILE/bell.wav
-    notify-send.sh --urgency=critical --expire-time=20000 -f --app-name=Reminder "Take a break" "Take a break for 20 second and look an object 20m far"
+    notify-send --urgency=normal --expire-time=20000 --app-name=Reminder "Take a break" "Take a break for 20 second and look an object 20m far"
+    mpv ~/Music/ding.mp3
     sleep 20
-
+    mpv ~/Music/ding.mp3
 done
-
